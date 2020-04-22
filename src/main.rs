52f596b8a6a0823a1761146ac5172b3f04bda24b
HAPPY <pcy190@126.com>
2020-04-22 21:19:22 +0800

    config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
