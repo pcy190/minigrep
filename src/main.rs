81070edaafa7b33d56b5a2303aff6e9ab855c15e
HAPPY <pcy190@126.com>
2020-04-23 00:01:37 +0800

    add test
    Signed-off-by: HAPPY <pcy190@126.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use std::env;
-use std::fs;
+use std::{env, process};
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Config { query, filename }
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Fail to read the file.");
-    println!("The content is: {}", contents);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error : {}", e);
+        process::exit(1);
+    }
 }
